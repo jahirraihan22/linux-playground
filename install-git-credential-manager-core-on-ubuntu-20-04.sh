df3ba97f9354e95c7cab4b1f83dde05e0be06f81
Jahir Raihan <39403552+jahirraihan22@users.noreply.github.com>
2022-07-30 09:35:11 +0600

    Add files via upload

---
+++ install-git-credential-manager-core-on-ubuntu-20-04.sh
@@ -0,0 +1,3 @@
+wget "https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.696/gcmcore-linux_amd64.2.0.696.deb" -O /tmp/gcmcore.deb
+sudo dpkg -i /tmp/gcmcore.deb
+git-credential-manager-core configure
