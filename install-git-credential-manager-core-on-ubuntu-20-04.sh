64d2769303f40bf873acf65d6d118ba1df041cb6
Jahir Raihan <39403552+jahirraihan22@users.noreply.github.com>
2022-07-30 10:15:42 +0600

    Update install-git-credential-manager-core-on-ubuntu-20-04.sh

--- install-git-credential-manager-core-on-ubuntu-20-04.sh
+++ install-git-credential-manager-core-on-ubuntu-20-04.sh
@@ -1,3 +1,4 @@
 wget "https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.696/gcmcore-linux_amd64.2.0.696.deb" -O /tmp/gcmcore.deb
 sudo dpkg -i /tmp/gcmcore.deb
 git-credential-manager-core configure
+git config --global credential.credentialStore secretservice
